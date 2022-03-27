fb3f4c2ef5af09a6320431cbe0c5a02b792bcbbe
Rohaizad Maznan <rohaizadmaznan@gmail.com>
2022-03-27 14:19:46 +0800

    AUO-2: Implement content of about us

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import Image from "next/image";
 
 function About() {
   return (
@@ -19,60 +20,55 @@ function About() {
         <div className="max-w-screen-xl px-4 md:px-8 mx-auto">
           <div className="grid md:grid-cols-2 gap-8 lg:gap-12">
             <div>
-              <div className="h-64 md:h-auto bg-gray-100 overflow-hidden rounded-lg shadow-lg">
-                <img
-                  src="https://images.unsplash.com/photo-1610465299996-30f240ac2b1c?auto=format&q=75&fit=crop&w=600&h=750"
-                  loading="lazy"
-                  alt="Photo by Martin Sanchez"
-                  className="w-full h-full object-cover object-center"
-                />
+              <div className="h-64 md:h-auto overflow-hidden rounded-lg shadow-lg">
+                <div className="w-full h-full object-cover object-center">
+                  <Image
+                    src="https://images.unsplash.com/photo-1503135935062-b7d1f5a0690f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                    alt="Love & roses"
+                    width={500}
+                    height={600}
+                    layout="responsive"
+                  />
+                </div>
               </div>
             </div>
 
             <div className="md:pt-8">
               <p className="text-[#ed639e] font-bold text-center md:text-left">
-                Who we are
+                The Among Us Only
               </p>
 
               <h1 className="text-2xl sm:text-3xl font-bold text-center md:text-left mb-4 md:mb-6">
-                Our competitive advantage
+                A collection of unsent messages for your loved ones.
               </h1>
 
               <p className="sm:text-lg mb-6 md:mb-8">
-                This is a section of some simple filler text, also known as
-                placeholder text. It shares some characteristics of a real
-                written text but is random or otherwise generated. It may be
-                used to display a sample of fonts or generate text for testing.
-                Filler text is dummy text which has no meaning however looks
-                very similar to real text.
+                The Among Us Only project is an achive collection of text
+                messages to first loves that have never been sent. Messages are
+                anonymously submitted from all over the world. Let the inner
+                feel be told as an annonymous.
                 <br />
                 <br />
-                This is a section of some simple filler text, also known as
-                placeholder text. It shares some characteristics of a real
-                written text but is{" "}
-                <a
-                  href="#"
-                  className=" text-indigo-500 hover:text-indigo-600 active:text-indigo-700 underline transition duration-100"
-                >
-                  random
-                </a>{" "}
-                or otherwise generated. It may be used to display a sample of
-                fonts or generate text for testing. Filler text is dummy text
-                which has no meaning however looks very similar to real text.
+                The submissions cover a wide range of topics, evoking nearly
+                every feeling imaginable. Messages have been sent to lovers,
+                best friends, exes, parents, siblings, and even pets, with the
+                term &quot;first love&quot; being subject to interpretation. All texts are
+                uploaded and preserved in a digital archive that may be searched
+                by name and colour. The Unsent Project strives to create a safe
+                haven for those who are experiencing mental health issues.
               </p>
-
-              <h2 className="text-[#ed639e] text-xl sm:text-2xl font-semibold text-center md:text-left mb-2 md:mb-4">
-                About us
-              </h2>
-
-              <p className="sm:text-lg mb-6 md:mb-8">
-                This is a section of some simple filler text, also known as
-                placeholder text. It shares some characteristics of a real
-                written text but is random or otherwise generated. It may be
-                used to display a sample of fonts or generate text for testing.
-                Filler text is dummy text which has no meaning however looks
-                very similar to real text.
-              </p>
+              <div className="max-w-4xl p-4 rounded-lg shadow">
+                <div className="mb-2">
+                  <div className="h-3 text-6xl text-left">&ldquo;</div>
+                  <p className="px-4 text-xl italic text-center">
+                    Love is heavy and light, bright and dark, hot and cold, sick
+                    and healthy, asleep and awake- its everything except what it
+                    is! &mdash;
+                    <span className="ml-2">William Shakespeare</span>
+                  </p>
+                  <div className="h-3 text-6xl text-right">&rdquo;</div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
