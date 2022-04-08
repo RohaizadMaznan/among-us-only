368c2f4f0bbbba481afa22ed8d92322222d253dd
rohaizadmaznan <rohaizadmaznan@gmail.com>
2022-04-09 00:18:39 +0800

    AUO-4: Added content in timeline

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -6,22 +6,24 @@ export const Timeline = () => {
       <ol className="relative border-l border-gray-200 dark:border-gray-700">
         <li className="mb-10 ml-4">
           <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-            February 2022
+          <time className="mb-1 text-sm font-normal leading-none dark:text-[#ed639e]">
+            Upcoming in 2022!
           </time>
-          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-            Application UI code in Tailwind CSS
+          <h3 className="text-xl font-semibold">
+            New Upcoming Planned Features
           </h3>
-          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-            Get access to over 20+ pages including a dashboard layout, charts,
-            kanban board, calendar, and pre-order E-commerce &amp; Marketing
-            pages.
+          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400 mt-2">
+            <ol className="list-disc ml-4">
+                <li>Improve search filters performance.</li>
+                <li>Save card into image format to local storage.</li>
+                <li>Store to print cards!</li>
+            </ol>
           </p>
           <a
-            href="#"
-            className="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+            href="https://github.com/RohaizadMaznan/among-us-only"
+            className="inline-flex items-center py-2 px-4 text-sm font-bold bg-white rounded-lg border border-gray-200 hover:bg-white hover:text-[#ed639e] focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
           >
-            Learn more{" "}
+            GitHub{" "}
             <svg
               className="ml-2 w-3 h-3"
               fill="currentColor"
@@ -36,31 +38,16 @@ export const Timeline = () => {
             </svg>
           </a>
         </li>
-        <li className="mb-10 ml-4">
-          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-            March 2022
-          </time>
-          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-            Marketing UI design in Figma
-          </h3>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-            All of the pages and components are first designed in Figma and we
-            keep a parity between the two versions even as we update the
-            project.
-          </p>
-        </li>
         <li className="ml-4">
           <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-            April 2022
+          <time className="mb-1 text-sm font-normal leading-none dark:text-[#ed639e]">
+            11 February 2022
           </time>
-          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-            E-Commerce UI code in Tailwind CSS
+          <h3 className="text-xl font-semibold">
+            Project launch 🚀
           </h3>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-            Get started with dozens of web components and interactive elements
-            built on top of Tailwind CSS.
+          <p className="text-base font-normal text-gray-500 dark:text-gray-400 mt-2">
+            The Among Us Only is officially launch and live. The audience can interact with the website to generate card on their own.
           </p>
         </li>
       </ol>
